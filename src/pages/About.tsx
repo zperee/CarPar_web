1426c77fe243cf5afa12c72eb0ff662c88d4e4fc
zperee <elia.perenzin@bluewin.ch>
2020-11-26 17:36:28 +0100

    Updated About page to same height

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -5,35 +5,35 @@ import IStyleSheet from "../shared/schemas/StyleSheet";
 export default function About(props: any) {
     return (
         <div className="container py-5">
-            <div className="row">
-                <div className="col-4">
+            <div className="row ">
+                <div className="col-4 heightFixed">
                     <Card>
                         <Card.Img variant="top" src="holder.js/100px180" alt="Image Daria"/>
                         <Card.Body>
                             <Card.Title>Daria Gloor</Card.Title>
-                            <Card.Text style={styles.heightFixed}>
+                            <Card.Text>
                                 Arbeitet bei und studiert digital Business Management
                             </Card.Text>
                         </Card.Body>
                     </Card>
                 </div>
-                <div className="col-4">
+                <div className="col-4 heightFixed">
                     <Card>
                         <Card.Img variant="top" src="holder.js/100px180" alt="Image Jessice"/>
                         <Card.Body>
                             <Card.Title>Jessica Nigg</Card.Title>
-                            <Card.Text style={styles.heightFixed}>
+                            <Card.Text className="heightFixed">
                                 Arbeitet bei und studiert digital Business Management
                             </Card.Text>
                         </Card.Body>
                     </Card>
                 </div>
-                <div className="col-4">
+                <div className="col-4 heightFixed">
                     <Card>
                         <Card.Img variant="top" src="holder.js/100px180" alt="Image Elia"/>
                         <Card.Body>
                             <Card.Title>Elia Perenzin</Card.Title>
-                            <Card.Text style={styles.heightFixed}>
+                            <Card.Text>
                                 Arbeitet bei der aimedic AG und studiert digital Business Management
                             </Card.Text>
                         </Card.Body>
@@ -43,9 +43,3 @@ export default function About(props: any) {
         </div>
     )
 }
-
-const styles: IStyleSheet = {
-    heightFixed: {
-        height: 70
-    },
-};
